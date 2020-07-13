5b9e899140d3e76db8882827d8c9b5fec4abf70a
P.J. Meisch <pj.meisch@sothawo.com>
2020-07-13 08:13:36 +0200

    add index template code to LogController

--- src/main/java/com/sothawo/springdataelastictest/LogController.java
+++ src/main/java/com/sothawo/springdataelastictest/LogController.java
@@ -4,10 +4,17 @@
 package com.sothawo.springdataelastictest;
 
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.IndexOperations;
 import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.index.DeleteTemplateRequest;
+import org.springframework.data.elasticsearch.core.index.ExistsTemplateRequest;
+import org.springframework.data.elasticsearch.core.index.GetTemplateRequest;
+import org.springframework.data.elasticsearch.core.index.PutTemplateRequest;
+import org.springframework.data.elasticsearch.core.index.TemplateData;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.data.elasticsearch.core.query.Criteria;
 import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,12 +32,16 @@ import java.time.ZonedDateTime;
 @RequestMapping("/logs")
 public class LogController {
 
+    private static final String TEMPLATE_NAME = "log-template";
+
     private final LogRepository logRepository;
     private final ElasticsearchOperations operations;
+    private final IndexOperations indexOps;
 
     public LogController(LogRepository logRepository, ElasticsearchOperations operations) {
         this.logRepository = logRepository;
         this.operations = operations;
+        this.indexOps = operations.indexOps(LogEntity.class);
     }
 
     //    @Scheduled(initialDelay = 5, fixedDelay = 15_000)
@@ -61,5 +72,29 @@ public class LogController {
         CriteriaQuery query = new CriteriaQuery(criteria);
 //        operations.getElasticsearchConverter().updateQuery(query, LogEntity.class);
         return operations.search(query, LogEntity.class, IndexCoordinates.of("log-*"));
+    }
+
+    @PostMapping("/template")
+    public boolean putTemplate() {
+        PutTemplateRequest putTemplateRequest = PutTemplateRequest.builder(TEMPLATE_NAME, "log-*")
+            .withSettings(indexOps.createSettings())
+            .withMappings(indexOps.createMapping())
+            .build();
+
+        return indexOps.putTemplate(putTemplateRequest);
+    }
+
+    @GetMapping("/template")
+    public ResponseEntity<TemplateData> getTemplate() {
+        if (indexOps.existsTemplate(new ExistsTemplateRequest(TEMPLATE_NAME))) {
+            return ResponseEntity.ok(indexOps.getTemplate(new GetTemplateRequest(TEMPLATE_NAME)));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/template")
+    public boolean deleteTemplate() {
+        return indexOps.deleteTemplate(new DeleteTemplateRequest(TEMPLATE_NAME));
     }
 }
