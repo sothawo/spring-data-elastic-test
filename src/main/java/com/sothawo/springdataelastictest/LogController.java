5607e813f7f7df15e374f07073e7bda052a99e6c
P.J. Meisch <pj.meisch@sothawo.com>
2020-07-17 20:51:42 +0200

    add LogController and related classes

---
+++ src/main/java/com/sothawo/springdataelastictest/LogController.java
@@ -0,0 +1,108 @@
+/*
+ * (c) Copyright 2020 sothawo
+ */
+package com.sothawo.springdataelastictest;
+
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.IndexOperations;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.index.AliasAction;
+import org.springframework.data.elasticsearch.core.index.AliasActionParameters;
+import org.springframework.data.elasticsearch.core.index.AliasActions;
+import org.springframework.data.elasticsearch.core.index.DeleteTemplateRequest;
+import org.springframework.data.elasticsearch.core.index.GetTemplateRequest;
+import org.springframework.data.elasticsearch.core.index.PutTemplateRequest;
+import org.springframework.data.elasticsearch.core.index.TemplateData;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+
+/**
+ * @author P.J. Meisch (pj.meisch@sothawo.com)
+ */
+@RestController
+@RequestMapping("/logs")
+public class LogController {
+
+    private static final String TEMPLATE_NAME = "log-template";
+
+    private final LogRepository logRepository;
+    private final ElasticsearchOperations operations;
+    private final IndexOperations indexOps;
+
+    public LogController(LogRepository logRepository, ElasticsearchOperations operations) {
+        this.logRepository = logRepository;
+        this.operations = operations;
+        this.indexOps = operations.indexOps(LogEntity.class);
+    }
+
+    //    @Scheduled(initialDelay = 5, fixedDelay = 15_000)
+    @PostMapping
+    public String log() {
+        LogEntity logEntity = new LogEntity();
+        String text = "Logging at " + LocalTime.now().toString();
+        logEntity.setText(text);
+        logRepository.save(logEntity);
+        return text;
+    }
+
+    @GetMapping("/{search}")
+    SearchHits<LogEntity> search(@PathVariable String search) {
+        return logRepository.searchByText(search);
+    }
+
+    @DeleteMapping
+    public void delete() {
+        logRepository.deleteAll();
+    }
+
+    @GetMapping("/between")
+    SearchHits<LogEntity> lastWeek() {
+        ZonedDateTime to = ZonedDateTime.now();
+        ZonedDateTime from = to.minusDays(7);
+        Criteria criteria = new Criteria("logTime").between(from, to);
+        CriteriaQuery query = new CriteriaQuery(criteria);
+//        operations.getElasticsearchConverter().updateQuery(query, LogEntity.class);
+        return operations.search(query, LogEntity.class, IndexCoordinates.of("log-*"));
+    }
+
+    @PostMapping("/template")
+    public boolean putTemplate() {
+        PutTemplateRequest putTemplateRequest = PutTemplateRequest.builder(TEMPLATE_NAME, "log-*")
+            .withSettings(indexOps.createSettings())
+            .withMappings(indexOps.createMapping())
+            .withAliasActions(new AliasActions().add(
+                new AliasAction.Add(AliasActionParameters.builderForTemplate()
+                    .withAliases("log-all")
+                    .build())))
+            .build();
+
+        return indexOps.putTemplate(putTemplateRequest);
+    }
+
+    @GetMapping("/template")
+    public ResponseEntity<TemplateData> getTemplate() {
+
+        TemplateData template = indexOps.getTemplate(new GetTemplateRequest(TEMPLATE_NAME));
+        if (template != null) {
+            return ResponseEntity.ok(template);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/template")
+    public boolean deleteTemplate() {
+        return indexOps.deleteTemplate(new DeleteTemplateRequest(TEMPLATE_NAME));
+    }
+}
