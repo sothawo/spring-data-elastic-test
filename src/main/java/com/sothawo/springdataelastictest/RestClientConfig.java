d5e04857b96cc5c80f950a2bedeaa5e968c9dc89
P.J. Meisch <pj.meisch@sothawo.com>
2020-08-08 21:30:04 +0200

    change ES port to 9400

--- src/main/java/com/sothawo/springdataelastictest/RestClientConfig.java
+++ src/main/java/com/sothawo/springdataelastictest/RestClientConfig.java
@@ -35,7 +35,7 @@ public class RestClientConfig extends AbstractElasticsearchConfiguration {
     public RestHighLevelClient elasticsearchClient() {
 
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder() //
-            .connectedTo("localhost:9200") //
+            .connectedTo("localhost:9400") //
 //            .usingSsl(NotVerifyingSSLContext.getSslContext()) //
             .withProxy("localhost:8080")
 //            .withPathPrefix("ela")
