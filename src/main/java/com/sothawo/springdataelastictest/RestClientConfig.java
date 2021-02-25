7d37be243971046369e12bb1d9e252dfcc16228e
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-02-25 18:34:42 +0100

    set ES port to 9200

--- src/main/java/com/sothawo/springdataelastictest/RestClientConfig.java
+++ src/main/java/com/sothawo/springdataelastictest/RestClientConfig.java
@@ -44,7 +44,7 @@ public class RestClientConfig extends AbstractElasticsearchConfiguration {
         };
 
         ClientConfiguration clientConfiguration = ClientConfiguration.builder() //
-            .connectedTo("localhost:9400") //
+            .connectedTo("localhost:9200") //
 //            .usingSsl()
 //            .usingSsl(NotVerifyingSSLContext.getSslContext()) //
             .withProxy("localhost:8080")
