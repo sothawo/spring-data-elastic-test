3e7807e750c28c631533b804eb368d7d12cc2607
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-03-22 20:52:20 +0100

    testing GraalVM native stuff

---
+++ src/main/java/com/sothawo/springdataelastictest/NativeConfiguration.java
@@ -0,0 +1,28 @@
+/*
+ * (c) Copyright 2021 sothawo
+ */
+package com.sothawo.springdataelastictest;
+
+import org.springframework.beans.factory.config.ObjectFactoryCreatingFactoryBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.annotations.HighlightField;
+import org.springframework.data.elasticsearch.core.event.AuditingEntityCallback;
+import org.springframework.nativex.hint.AccessBits;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.TypeHint;
+
+/**
+ * @author P.J. Meisch (pj.meisch@sothawo.com)
+ */
+
+@NativeHint(
+    types = {
+        @TypeHint(types = HighlightField.class, access = AccessBits.DECLARED_METHODS),
+        @TypeHint(types = {AuditingEntityCallback.class}),
+        @TypeHint(typeNames = {"com.sothawo.springdataelastictest.person.PersonCustomRepositoryImpl"}, access = AccessBits.ALL),
+        @TypeHint(types = {ObjectFactoryCreatingFactoryBean.class})
+    }
+)
+@Configuration
+public class NativeConfiguration {
+}
