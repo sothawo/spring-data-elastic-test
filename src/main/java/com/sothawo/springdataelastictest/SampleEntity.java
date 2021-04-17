0bb9e91fbd253b72d206b4dacff4738b2de00275
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-04-17 14:15:55 +0200

    soe tests with SampleEntity

--- src/main/java/com/sothawo/springdataelastictest/SampleEntity.java
+++ src/main/java/com/sothawo/springdataelastictest/SampleEntity.java
@@ -5,8 +5,12 @@ package com.sothawo.springdataelastictest;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.DynamicMapping;
+import org.springframework.data.elasticsearch.annotations.DynamicMappingValue;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.Map;
 
 @Document(indexName = "sample-entities")
 public class SampleEntity {
@@ -15,6 +19,10 @@ public class SampleEntity {
 
     @Field(type = FieldType.Text)
     private String message;
+
+    @Field(type = FieldType.Object)
+    @DynamicMapping(DynamicMappingValue.False)
+    private Map<String, Object> objectMap;
 
     public String getId() {
         return id;
@@ -30,5 +38,13 @@ public class SampleEntity {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public Map<String, Object> getObjectMap() {
+        return objectMap;
+    }
+
+    public void setObjectMap(Map<String, Object> objectMap) {
+        this.objectMap = objectMap;
     }
 }
