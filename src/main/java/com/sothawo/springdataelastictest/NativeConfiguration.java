37f954505106ae9868f88ac2449634072d7b64e6
P.J. Meisch <peter-josef.meisch@codecentric.de>
2021-04-30 16:04:43 +0200

    adapt versions, native configuration

--- src/main/java/com/sothawo/springdataelastictest/NativeConfiguration.java
+++ src/main/java/com/sothawo/springdataelastictest/NativeConfiguration.java
@@ -5,10 +5,13 @@ package com.sothawo.springdataelastictest;
 
 import org.springframework.beans.factory.config.ObjectFactoryCreatingFactoryBean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.AuditingHandlerSupport;
+import org.springframework.data.auditing.IsNewAwareAuditingHandler;
 import org.springframework.data.elasticsearch.annotations.HighlightField;
 import org.springframework.data.elasticsearch.core.event.AuditingEntityCallback;
 import org.springframework.nativex.hint.AccessBits;
 import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.ResourceHint;
 import org.springframework.nativex.hint.TypeHint;
 
 /**
@@ -16,11 +19,18 @@ import org.springframework.nativex.hint.TypeHint;
  */
 
 @NativeHint(
-    types = {
-        @TypeHint(types = HighlightField.class, access = AccessBits.DECLARED_METHODS),
-        @TypeHint(types = {AuditingEntityCallback.class}),
-        @TypeHint(types = {ObjectFactoryCreatingFactoryBean.class})
-    }
+        types = {
+                @TypeHint(types = HighlightField.class, access = AccessBits.DECLARED_METHODS),
+                @TypeHint(typeNames = {"org.springframework.data.elasticsearch.config.PersistentEntitiesFactoryBean"}),
+                @TypeHint(types = {ObjectFactoryCreatingFactoryBean.class}),
+                @TypeHint(types = {AuditingEntityCallback.class}),
+                @TypeHint(types = {AuditingHandlerSupport.class}),
+                @TypeHint(types = {IsNewAwareAuditingHandler.class})
+        },
+        resources = {
+                // for the Faker data
+                @ResourceHint( patterns = {".*.yml", "en/.*.yml"})
+        }
 )
 @Configuration
 public class NativeConfiguration {
