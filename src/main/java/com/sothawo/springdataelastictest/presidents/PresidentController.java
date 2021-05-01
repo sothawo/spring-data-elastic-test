9c3d15aa9ac5f9158264963fe82cdc8f758a96f9
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-05-01 14:05:42 +0200

    some presidental work

--- src/main/java/com/sothawo/springdataelastictest/presidents/PresidentController.java
+++ src/main/java/com/sothawo/springdataelastictest/presidents/PresidentController.java
@@ -3,10 +3,15 @@
  */
 package com.sothawo.springdataelastictest.presidents;
 
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -16,29 +21,41 @@ import java.util.Arrays;
 public class PresidentController {
 
     private final PresidentRepository repository;
+    private final ElasticsearchOperations operations;
 
-    public PresidentController(PresidentRepository repository) {
+    public PresidentController(PresidentRepository repository, ElasticsearchOperations operations) {
         this.repository = repository;
+        this.operations = operations;
     }
 
     @GetMapping("/load")
     public void load() {
         repository.saveAll(Arrays.asList(
-                President.of("Dwight D Eisenhower", 1953, 1961),
-                President.of("Lyndon B Johnson", 1963, 1969),
-                President.of("Richard Nixon", 1969, 1974),
-                President.of("Gerald Ford", 1974, 1977),
-                President.of("Jimmy Carter", 1977, 1981),
-                President.of("Ronald Reagen", 1981, 1989),
-                President.of("George Bush", 1989, 1993),
-                President.of("Bill Clinton", 1993, 2001),
-                President.of("George W Bush", 2001, 2009),
-                President.of("Barack Obama", 2009, 2017),
-                President.of("Donald Trump", 2017, 2021)));
+            President.of("Dwight D Eisenhower", 1953, 1961),
+            President.of("Lyndon B Johnson", 1963, 1969),
+            President.of("Richard Nixon", 1969, 1974),
+            President.of("Gerald Ford", 1974, 1977),
+            President.of("Jimmy Carter", 1977, 1981),
+            President.of("Ronald Reagen", 1981, 1989),
+            President.of("George Bush", 1989, 1993),
+            President.of("Bill Clinton", 1993, 2001),
+            President.of("George W Bush", 2001, 2009),
+            President.of("Barack Obama", 2009, 2017),
+            President.of("Donald Trump", 2017, 2021),
+            President.of("Joe Biden", 2021, 2025)));
     }
 
     @GetMapping("/term/{year}")
     public SearchHits<President> searchByTerm(@PathVariable Integer year) {
         return repository.searchByTerm(year);
+    }
+
+    @GetMapping("/name/{name}")
+    public SearchHits<President> searchByName(@PathVariable String name, @Nullable @RequestParam(required = false) Boolean requestCache) {
+        var query = new CriteriaQuery(new Criteria("name").is(name));
+        if (requestCache != null) {
+            query.setRequestCache(requestCache);
+        }
+        return operations.search(query, President.class);
     }
 }
