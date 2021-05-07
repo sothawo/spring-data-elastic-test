fe289c060cd9d6d70692dc1020b808c59f07abad
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-05-07 19:08:41 +0200

    fix router and handler setup for presidents

--- src/main/java/com/sothawo/springdataelastictest/presidents/PresidentRouter.java
+++ src/main/java/com/sothawo/springdataelastictest/presidents/PresidentRouter.java
@@ -3,17 +3,20 @@ package com.sothawo.springdataelastictest.presidents;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.*;
 
 @Configuration("PresidentRouterConfiguration")
 class PresidentRouter {
 
     @Bean
     public RouterFunction<ServerResponse> presidentRouter(PresidentHandler handler) {
-        return RouterFunctions
-                .route(GET("/preesidents/load"), handler::load);
+        return nest(path("/presidents"),
+            route(GET("/load"), handler::load)
+                .andRoute(GET("/name/{name}"), handler::searchByName)
+                .andRoute(GET("/term/{year}"), handler::searchByTerm)
+                .andRoute(GET("/{id}"), handler::byId));
     }
 }
