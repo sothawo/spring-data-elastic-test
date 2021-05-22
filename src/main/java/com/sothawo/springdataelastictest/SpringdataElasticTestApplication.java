30da22d592aa00c5232e01c4b11953e4992ac660
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-05-22 12:30:42 +0200

    remove some blockhound stuff, add version logging to app startup code

--- src/main/java/com/sothawo/springdataelastictest/SpringdataElasticTestApplication.java
+++ src/main/java/com/sothawo/springdataelastictest/SpringdataElasticTestApplication.java
@@ -1,38 +1,44 @@
 package com.sothawo.springdataelastictest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.domain.ReactiveAuditorAware;
 import org.springframework.data.elasticsearch.config.EnableReactiveElasticsearchAuditing;
+import org.springframework.data.elasticsearch.core.ReactiveElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ReactiveElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableReactiveElasticsearchRepositories;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.userdetails.User;
 import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingOperationError;
 
 @SpringBootApplication(exclude = ElasticsearchDataAutoConfiguration.class)
 @EnableReactiveElasticsearchRepositories
 @EnableReactiveElasticsearchAuditing
 public class SpringdataElasticTestApplication {
 
+    @Autowired
+    private ReactiveElasticsearchOperations operations;
+
     static {
         BlockHound.install(
-            builder -> builder.allowBlockingCallsInside("org.springframework.data.elasticsearch.support.VersionInfo", "logVersions"),
-            builder -> builder.allowBlockingCallsInside("org.springframework.data.elasticsearch.core.index.MappingBuilder", "addRuntimeFields"),
             builder -> builder.allowBlockingCallsInside("org.elasticsearch.Build", "<clinit>"),
             builder -> builder.allowBlockingCallsInside("org.elasticsearch.common.xcontent.XContentBuilder", "<clinit>"),
             builder -> builder.allowBlockingCallsInside("com.github.javafaker.service.FakeValues", "loadValues"),
             builder -> builder.blockingMethodCallback(it -> {
-                new Error(it.toString()).printStackTrace();
+                throw new BlockingOperationError(it);
             })
         );
     }
 
     public static void main(String[] args) {
-
         SpringApplication.run(SpringdataElasticTestApplication.class, args);
     }
 
@@ -44,5 +50,10 @@ public class SpringdataElasticTestApplication {
             .map(Authentication::getPrincipal)
             .map(User.class::cast)
             .map(User::getUsername);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    void logElasticVersions() {
+        ((ReactiveElasticsearchTemplate) operations).logVersions().subscribe();
     }
 }
