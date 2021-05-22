cb0ba5fb3daa717294cb05b3be0433b62c53e4ad
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-05-22 15:24:04 +0200

    add logging of versions on startup

--- src/main/java/com/sothawo/springdataelastictest/SpringdataElasticTestApplication.java
+++ src/main/java/com/sothawo/springdataelastictest/SpringdataElasticTestApplication.java
@@ -1,11 +1,16 @@
 package com.sothawo.springdataelastictest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.elasticsearch.config.EnableElasticsearchAuditing;
+import org.springframework.data.elasticsearch.core.AbstractElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.core.Authentication;
@@ -19,6 +24,9 @@ import java.util.Optional;
 @EnableElasticsearchAuditing
 @EnableScheduling
 public class SpringdataElasticTestApplication {
+
+    @Autowired
+    private ElasticsearchOperations operations;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringdataElasticTestApplication.class, args);
@@ -35,4 +43,10 @@ public class SpringdataElasticTestApplication {
             return Optional.ofNullable(userName);
         };
     }
+
+    @EventListener(ApplicationReadyEvent.class)
+    void logElasticVersions() {
+        ((AbstractElasticsearchTemplate) operations).logVersions();
+    }
+
 }
