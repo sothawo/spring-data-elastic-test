c677e6f83cb180c67d740d70381951dd9b7e8afa
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-05-24 17:50:07 +0200

    some SO tests

--- src/main/java/com/sothawo/springdataelastictest/so/FooController.java
+++ src/main/java/com/sothawo/springdataelastictest/so/FooController.java
@@ -4,13 +4,20 @@
 package com.sothawo.springdataelastictest.so;
 
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ScriptType;
 import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.ByQueryResponse;
+import org.springframework.data.elasticsearch.core.query.Query;
+import org.springframework.data.elasticsearch.core.query.UpdateQuery;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author P.J. Meisch (pj.meisch@sothawo.com)
@@ -40,5 +47,13 @@ public class FooController {
     @GetMapping
     public SearchHits<Foo> all() {
         return fooRepository.searchBy();
+    }
+
+    @GetMapping("/test")
+    public void test() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("answer", 42);
+        var updateQuery = UpdateQuery.builder(Query.findAll()).withScriptType(ScriptType.STORED).withScriptName("fooScript").withParams(params).build();
+        var response = operations.updateByQuery(updateQuery, operations.getIndexCoordinatesFor(Foo.class));
     }
 }
