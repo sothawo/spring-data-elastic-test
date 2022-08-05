8f775a52c7ddf16dfca58f0a39065ff1919e7591
Peter-Josef Meisch <pj.meisch@sothawo.com>
2022-08-05 21:01:10 +0200

    enable boot autoconfig to see if there are errors

--- src/main/java/com/sothawo/springdataelastictest/SpringdataElasticTestApplication.java
+++ src/main/java/com/sothawo/springdataelastictest/SpringdataElasticTestApplication.java
@@ -3,7 +3,6 @@ package com.sothawo.springdataelastictest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
@@ -19,34 +18,34 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import java.util.Optional;
 
 
-@SpringBootApplication(exclude = ElasticsearchDataAutoConfiguration.class)
+@SpringBootApplication
 @EnableElasticsearchRepositories(namedQueriesLocation = "classpath:named-queries.properties")
 @EnableElasticsearchAuditing
 @EnableScheduling
 public class SpringdataElasticTestApplication {
 
-    @Autowired
-    private ElasticsearchOperations operations;
+	@Autowired
+	private ElasticsearchOperations operations;
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringdataElasticTestApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataElasticTestApplication.class, args);
+	}
 
-    @Bean
-    AuditorAware<String> auditorAware() {
-        return () -> {
-            String userName = null;
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            if (authentication != null) {
-                userName = authentication.getName();
-            }
-            return Optional.ofNullable(userName);
-        };
-    }
+	@Bean
+	AuditorAware<String> auditorAware() {
+		return () -> {
+			String userName = null;
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			if (authentication != null) {
+				userName = authentication.getName();
+			}
+			return Optional.ofNullable(userName);
+		};
+	}
 
-    @EventListener(ApplicationReadyEvent.class)
-    void logElasticVersions() {
-        ((AbstractElasticsearchTemplate) operations).logVersions();
-    }
+	@EventListener(ApplicationReadyEvent.class)
+	void logElasticVersions() {
+		((AbstractElasticsearchTemplate) operations).logVersions();
+	}
 
 }
