5c9a31aba769b88def7b2199137714ef0771f078
Peter-Josef Meisch <pj.meisch@sothawo.com>
2023-09-03 13:48:55 +0200

    added nanoseconds timestamp to a class

--- src/main/java/com/sothawo/springdataelastictest/sampleentity/SampleEntity.java
+++ src/main/java/com/sothawo/springdataelastictest/sampleentity/SampleEntity.java
@@ -4,13 +4,7 @@
 package com.sothawo.springdataelastictest.sampleentity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.annotation.PersistenceConstructor;
-import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.annotation.*;
 import org.springframework.data.domain.Persistable;
 import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
@@ -20,60 +14,84 @@ import org.springframework.data.elasticsearch.core.query.SeqNoPrimaryTerm;
 import org.springframework.lang.Nullable;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Document(indexName = "sample-entities")
 public record SampleEntity(
-    @Id
-    @Nullable
-    String id,
+				@Id
+				@Nullable
+				String id,
 
-    @Field(type = FieldType.Text)
-    @Nullable String message,
+				@Field(type = FieldType.Text)
+				@Nullable String message,
 
-    @CreatedDate
-    @Field(type = FieldType.Date, format = DateFormat.basic_date_time)
-    @Nullable
-    Instant created,
+				@CreatedDate
+				@Field(type = FieldType.Date, format = DateFormat.basic_date_time)
+				@Nullable
+				Instant created,
 
-    @CreatedBy
-    @Nullable
-    String createdBy,
+				@CreatedBy
+				@Nullable
+				String createdBy,
 
-    @LastModifiedDate
-    @Field(type = FieldType.Date, format = DateFormat.basic_date_time)
-    @Nullable
-    Instant lastModified,
+				@LastModifiedDate
+				@Field(type = FieldType.Date, format = DateFormat.basic_date_time)
+				@Nullable
+				Instant lastModified,
 
-    @LastModifiedBy
-    @Nullable
-    String lastModifiedBy,
+				@LastModifiedBy
+				@Nullable
+				String lastModifiedBy,
 
-    @Field(type = FieldType.Nested)
-    List<Object> objects,
+				@Field(type = FieldType.Nested)
+				List<Object> objects,
 
-    @Nullable SeqNoPrimaryTerm seqNoPrimaryTerm
+				@Nullable SeqNoPrimaryTerm seqNoPrimaryTerm,
+
+				@Field(type = FieldType.Date_Nanos, format = DateFormat.strict_date_optional_time_nanos)
+				@Nullable Instant timestamp
+
 ) implements Persistable<String> {
-    @PersistenceCreator
-    public SampleEntity {
-    }
+		@PersistenceCreator
+		public SampleEntity {
+				if (timestamp == null) {
+						timestamp = Instant.now();
+				}
+		}
 
-    public SampleEntity(@Nullable String id, List<Object> objects) {
-        this(id, null, null, null, null, null, objects, null);
-    }
+		public SampleEntity(@Nullable String id, List<Object> objects) {
+				this(id,
+								null,
+								null,
+								null,
+								null,
+								null,
+								objects,
+								null,
+								null);
+		}
 
-    @Override
-    public String getId() {
-        return id;
-    }
+		@Override
+		public String getId() {
+				return id;
+		}
 
-    @JsonIgnore
-    @Override
-    public boolean isNew() {
-        return id == null || (createdBy == null && created == null);
-    }
+		@JsonIgnore
+		@Override
+		public boolean isNew() {
+				return id == null || (createdBy == null && created == null);
+		}
 
-    public SampleEntity withId(String id) {
-        return new SampleEntity(id, this.message, this.created, this.createdBy, this.lastModified, this.lastModifiedBy, this.objects, this.seqNoPrimaryTerm);
-    }
+		public SampleEntity withId(String id) {
+				return new SampleEntity(id,
+								this.message,
+								this.created,
+								this.createdBy,
+								this.lastModified,
+								this.lastModifiedBy,
+								this.objects,
+								this.seqNoPrimaryTerm,
+								this.timestamp);
+		}
 }
