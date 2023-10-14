c6b83d280a828f26dd85a93e7c61d98a357c6ab0
Peter-Josef Meisch <pj.meisch@sothawo.com>
2023-10-14 13:19:03 +0200

    different refresh policy tests

--- src/main/java/com/sothawo/springdataelastictest/foo/FooService.java
+++ src/main/java/com/sothawo/springdataelastictest/foo/FooService.java
@@ -3,34 +3,45 @@
  */
 package com.sothawo.springdataelastictest.foo;
 
+import org.springframework.data.elasticsearch.core.RefreshPolicy;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.time.Duration;
-import java.time.Instant;
 
 /**
  * @author P.J. Meisch (pj.meisch@sothawo.com)
  */
 @Service
 public class FooService {
-	private final FooRepository repository;
+		private final FooRepository repository;
 
-	public FooService(FooRepository repository) {
-		this.repository = repository;
-	}
+		public FooService(FooRepository repository) {
+				this.repository = repository;
+		}
 
-public Mono<Void> test() {
-	var start = Instant.now();
+		public Mono<Void> test() {
+				var foo = new Foo();
 
-	var fooFlux =  Flux.range(1, 1_000_000)
-		.map(Foo::of);
-
-	return repository.saveAll(fooFlux)
-		.doOnComplete(() -> {
-			System.out.println(Duration.between(start, Instant.now()));
-		})
-		.then();
-}
+				foo.setId("1");
+				foo.setText("immediate - default");
+				return repository.save(foo)
+								.flatMap(f -> {
+														foo.setId("2");
+														foo.setText("none");
+														return repository.save(foo, RefreshPolicy.NONE);
+												}
+								)
+								.flatMap(f -> {
+														foo.setId("3");
+														foo.setText("wait_until");
+														return repository.save(foo, RefreshPolicy.WAIT_UNTIL);
+												}
+								)
+								.flatMap(f -> {
+														foo.setId("4");
+														foo.setText("immediate - default");
+														return repository.save(foo);
+												}
+								)
+								.then();
+		}
 }
